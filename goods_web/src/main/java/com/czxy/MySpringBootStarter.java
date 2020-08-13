9b57105c27c4e06bd3c4e80380426cb089c37bc6
王培岛 <q543632148@163.com>
2020-08-13 08:44:00 +0800

    添加注释

--- goods_web/src/main/java/com/czxy/MySpringBootStarter.java
+++ goods_web/src/main/java/com/czxy/MySpringBootStarter.java
@@ -3,7 +3,10 @@ package com.czxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ *
+ * 2020-08-12
+ */
 @SpringBootApplication
 public class MySpringBootStarter {
     public static void main(String[] args) {
